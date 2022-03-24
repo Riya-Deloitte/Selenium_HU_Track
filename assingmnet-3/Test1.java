29ba20ffd23f90a8acceb0f24470ffb686c93515
Riya-Deloitte <101085440+Riya-Deloitte@users.noreply.github.com>
2022-03-24 11:21:05 +0530

    [+]

---
+++ assingmnet-3/Test1.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\riyasingh6\\browserdriver\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com");
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
+        driver.get("https://the-internet.herokuapp.com/frames");
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+        String title = driver.getTitle();
+        System.out.println(title);
+
+    }
+
+}
